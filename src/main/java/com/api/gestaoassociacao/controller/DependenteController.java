d7ec3d5e2d86c0cefb848421b091ae94a729b9ba
dayannaclaudino <day_claudino@hotmail.com>
2023-11-23 18:15:34 -0300

    Incluir dependente ao Associado Refeita
    Agora a partir de um associado é incluído o Dependente

--- src/main/java/com/api/gestaoassociacao/controller/DependenteController.java
+++ src/main/java/com/api/gestaoassociacao/controller/DependenteController.java
@@ -1,36 +1,47 @@
 package com.api.gestaoassociacao.controller;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.api.gestaoassociacao.model.Associado;
 import com.api.gestaoassociacao.model.Dependente;
+import com.api.gestaoassociacao.repository.AssociadoRepository;
+import com.api.gestaoassociacao.repository.DependenteRepository;
 import com.api.gestaoassociacao.service.AssociadoService;
 import com.api.gestaoassociacao.service.DependenteService;
 
 import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping("dependentes")
 public class DependenteController {
 
     private static final String VIEW = "cadastroDependentes";
 
     @Autowired
     private DependenteService dependenteService;
-
+    @Autowired
+    private DependenteRepository dependenteRepository;
     @Autowired
     private AssociadoService associadoService;
+    @Autowired
+    private AssociadoRepository associadoRepository;
 
 
 
-    @GetMapping("/dependentes")
+
+    @GetMapping("/listar")
     public ModelAndView getDependentes(){
         
         ModelAndView mv = new ModelAndView(VIEW);
@@ -42,11 +53,20 @@ public class DependenteController {
         return mv;
     }
 
-    @PostMapping("/salvar")
-    public String salvar(@ModelAttribute Dependente dependente){
+    @PostMapping("/addDepenAssociado/{codigo}")
+    public ModelAndView addDepenAssociado(Dependente dependente, @PathVariable("codigo") Long id, 
+                                          BindingResult result, RedirectAttributes attributes){
+        Associado associado = associadoRepository.findById(id).get();
+      
 
-        dependenteService.inserir(dependente);
-        return "redirect:/dependentes";
+        ModelAndView mv = new ModelAndView(VIEW);
+        dependente.setAssociado(associado);                                        
+        dependenteService.inserir(dependente); 
+                                           
+        mv.addObject("associado", associado);
+        mv.addObject("dependentes", dependenteRepository.getDependentes(id));                                  
+       
+        return mv;
     }
 
    
